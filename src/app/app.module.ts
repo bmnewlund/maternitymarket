12641a161b3da9697550bc3bf90a4527e1ac6457
simonhlee97 <simonhlee97@hotmail.com>
2017-10-10 13:59:30 -0400

    Tuesday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+//import { ViewAllComponent } from './view-all/view-all.component';
+import { ShopComponent } from './shop/shop.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { RouterModule } from "@angular/router";
+import { routes } from "./constants/routes";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    //ViewAllComponent,
+    ShopComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     FormsModule,
     HttpModule
   ],
--- src/app/constants/routes.ts
+++ src/app/constants/routes.ts
@@ -1,12 +1,16 @@
 import {Routes, RouterModule} from "@angular/router";
 import { HomeComponent } from "../home/home.component";
-import { ViewAllComponent } from "../view/viewAll.component";
+// import { ViewAllComponent } from "../view-all/viewAll.component";
 import { LoginComponent } from "../login/login.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "app/footer/footer.component";
 
 export const routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},  
-  {path: 'viewAll', component: ViewAllComponent},
+  //{path: 'login', component: LoginComponent},
+  //{path: 'footer', component: FooterComponent},  
+  
+ //  {path: 'viewAll', component: ViewAllComponent},
   {path: '**', component: HomeComponent}
 ];
