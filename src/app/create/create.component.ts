a46597f421ffd2f1aea93de228465b096b1961a7
simonhlee97 <simonhlee97@hotmail.com>
2017-10-16 14:20:33 -0400

    angular version 4 and firebase data persists

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http } from '@angular/http';
+import { ItemService } from './item.service';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 
-export class CreateComponent implements OnInit {
-  constructor() {
-    console.log('constructor ran..');
-  }
-  ngOnInit() {
-    console.log('ngOnInit ran...');
+export class CreateComponent {
+  
+  constructor(private itemService: ItemService) {}
+  addItem(title, description, zip) {
+    let data = {
+      title: title.value,
+      description: description.value,
+      zip: zip.value
+    }
+    console.log(title.value, description.value, zip.value)
+    this.itemService.AddItems(data)
+    .subscribe(
+      (response) => console.log(response)
+    );
   }
 
 }
