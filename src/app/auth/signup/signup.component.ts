106693dcc9aef6d9cee33df9162d3b4446441388
Brett Newlund <bmnewlund@gmail.com>
2017-10-18 16:04:37 -0400

    Added Signup/Users

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'app/auth/auth.service';
-
+import { Headers, Http, Response } from '@angular/http';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,7 +9,7 @@ import { AuthService } from 'app/auth/auth.service';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private http: Http) { }
 
   ngOnInit() {
   }
@@ -18,5 +18,24 @@ export class SignupComponent implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
     this.authService.signupUser(email, password);
+    
+  
+    let data = {
+      email: form.value.email,
+      phone: form.value.phone,
+      zip: form.value.zip
+    }
+console.log(data)
+
+
+    
+    return this.http.post('https://maternity1150.firebaseio.com/users.json',
+    data).subscribe();
+
+
+
+      
+    }
+  
   }
-}
+
