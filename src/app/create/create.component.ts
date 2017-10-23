e80538bc25009b6ddebb1d5098bfd599e39a7395
Brandon Schaaf <brandonschaaf@gmail.com>
2017-10-23 09:17:08 -0400

    item add

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -11,15 +11,14 @@ import 'rxjs/add/operator/map';
 export class CreateComponent {
   
   constructor(private itemService: ItemService) {}
-  createItem(category, title, price, description, zip) {
+  createItem(category, title, price, description) {
     let data = {
       category: category.value,
       title: title.value,
-      description: description.value,
       price: price.value,
-      zip: zip.value
+      description: description.value
     }
-    console.log(category.value, title.value, price.value, description.value, zip.value)
+    console.log(category.value, title.value, price.value, description.value)
     this.itemService.createItem(data)
     // .subscribe(
     //   (response) => console.log(response)
