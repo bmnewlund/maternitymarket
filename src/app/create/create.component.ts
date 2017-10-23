064c6293063404808daace3a04f40b420a4d02c2
Brett Newlund <bmnewlund@gmail.com>
2017-10-23 15:12:31 -0400

    Added Image

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { ItemService } from './item.service';
 import 'rxjs/add/operator/map';
+import * as firebase from 'firebase';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -10,7 +13,7 @@ import 'rxjs/add/operator/map';
 
 export class CreateComponent {
   
-  constructor(private itemService: ItemService) {}
+  constructor(private itemService: ItemService, db: AngularFireDatabase) {}
   createItem(category, title, price, description) {
     let data = {
       category: category.value,
@@ -24,5 +27,18 @@ export class CreateComponent {
     //   (response) => console.log(response)
     // );
   }
+
+  
+
+  itemImageSelected(event: any) {
+    const file: File = event.target.files[0];
+    console.log("Selected filename: ", file.name);
+
+    const metaData = {'contentType': file.type};
+    const storageRef: firebase.storage.Reference = firebase.storage().ref(('/photos/featured/url1'))
+    storageRef.put(file,metaData);
+    console.log("Uploading", file.name);
+  }
+
 
 }
