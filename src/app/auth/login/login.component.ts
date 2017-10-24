cc65f2a91c06cca0d7845eb8b5c7a32e23ce7dcb
Simon Lee <simonhlee97@hotmail.com>
2017-10-24 09:16:15 -0400

    Login form validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators, FormBuilder, NgForm } from '@angular/forms';
 import { AuthService } from 'app/auth/auth.service';
-import * as firebase from 'firebase'
+import * as firebase from 'firebase';
+import {Router, RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -10,16 +11,43 @@ import * as firebase from 'firebase'
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  myform: FormGroup;
+  email: FormControl;
+  password: FormControl;
+  
+  createFormControls() {
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+    
+    
+  }
+  createForm() {
+    this.myform = new FormGroup({
+      email: this.email,
+      password: this.password
+    });
   }
 
   onSignin(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
     this.authService.signinUser(email, password).then(() => console.log(firebase.auth().currentUser.email));
-    
+    this.router.navigate(['admin'])
+    alert('You Are Logged In!');
+    console.log('Thanks for logging in!');
   }
 
 }
